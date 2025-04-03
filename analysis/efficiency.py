5475d98b1c09954c0fb8d801fd77717427296b79
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2025-04-03 17:49:39 +0200

    improved efficiency vis

--- analysis/efficiency.py
+++ analysis/efficiency.py
@@ -15,7 +15,8 @@ def to_xarray(a):
 	return xr.DataArray(a[:,1], {"wavelength":a[:,0]})
 
 reference = to_xarray(np.loadtxt("../measurement/2025-04-03/001.txt"))
-spectrum = to_xarray(np.loadtxt("../measurement/2025-04-03/002.asc"))
+spectrum = to_xarray(np.loadtxt("../measurement/2025-04-03/002.asc")).sel(wavelength=slice(220, None))
+assert np.count_nonzero(spectrum > 1e5)==0
 spectrum = spectrum.rolling(wavelength=100).mean() # filter the spectrum
 spectrum -= 23895 # recorded background
 
@@ -25,11 +26,10 @@ eff = eff / eff.sel(wavelength=slice(300, None)).max() # normalize on peak
 # import the manufactures curve
 man = to_xarray(np.loadtxt("../measurement/2025-04-03/QEcurve.dat")).sortby("wavelength")
 
-norm = lambda a: a/a.sel(wavelength=slice(400, 600)).max()
-
 ax1 = plt.subplot(2, 1, 1)
-norm(reference).plot(label="Ocean Optics")
-norm(spectrum).plot(label="Andor Solis")
+(reference/1e4).plot(label="Ocean Optics")
+norm = spectrum * (reference.interp(wavelength=spectrum.wavelength)/spectrum).sel(wavelength=slice(300,None)).min()
+(norm/1e4).plot(label="Andor Solis")
 plt.ylabel("Intensity")
 plt.legend()
 
@@ -37,8 +37,11 @@ ax2 = plt.subplot(2, 1, 2, sharex=ax1)
 man.plot(label="Manufacturer", color="C2")
 (eff*100).plot(label="Measured", color="C3")
 plt.ylabel("Efficiency (%)")
+plt.xlabel(r"$\lambda$ (nm)")
 plt.ylim(0, 100)
+plt.axvline(500, label="Grating Optimized", color="C4")
 plt.legend()
 ax1.label_outer()
 
 plt.gcf().savefig("figures/efficiency.pdf")
+# %%
