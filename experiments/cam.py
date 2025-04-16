906cb726baffd9555d661750e65362bfc5a6d0a8
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2025-04-16 13:55:13 +0200

    camera module

--- experiments/cam.py
+++ experiments/cam.py
@@ -1,46 +1,71 @@
-# %%
 from pylablib.devices import Andor
-import matplotlib.pyplot as plt
-# plt.style.use("../style.mplstyle")
-import matplotlib as mpl
-mpl.use("Qt5Agg")
 
-#%%
-cam = Andor.AndorSDK2Camera(temperature=-80, fan_mode="full")
+class Camera:
+    def __init__(self):
+        self.cam = Andor.AndorSDK2Camera(fan_mode="full")
+        self.cam.set_read_mode("image")
+        self.temperature = -80
 
-cam.set_temperature(-80)
-cam.get_temperature()
+    @property
+    def temperature(self):
+        return self.cam.get_temperature()
 
-cam.set_read_mode("image")
-# cam.get_capabilities()
+    @temperature.setter
+    def temperature(self, value):
+        self.cam.set_temperature(value, enable_cooler=True)
 
+    @property
+    def exposure(self):
+        return self.cam.get_exposure()
+    
+    @exposure.setter
+    def exposure(self, value):
+        self.cam.set_exposure(value)
 
-# cam.setup_shutter("closed")
-# cam.setup_shutter("open")
+    def __call__(self):
+        return self.cam.grab(frame_timeout=self.exposure+1)[0]
 
-cam.set_exposure(0.1)
+    def wait_for_cooldown(self):
+        while self.temperature > self.cam.get_temperature_setpoint():
+            pass
 
-print("camera ready")
-
-# %%
-
-def frame():
-    return cam.grab(frame_timeout=999)[0]
-
-plt.ion()
-
-fig, ax = plt.subplots(1,1)
-img = ax.imshow(frame())
-plt.show()
-
-def loop():
-    img.set_data(frame())
-    fig.canvas.draw()
-    fig.canvas.flush_events()
-
-while True: loop()
+    def __del__(self):
+        self.cam.close()
 
 
-#%%
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import yaml
 
-cam.close()
+    with open("config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    plt.ion()
+
+    cam = Camera()
+    cam.exposure = config["exposure"]
+
+
+    fig, ax = plt.subplots()
+    img = ax.imshow(cam())
+    plt.colorbar(img, ax=ax)
+    plt.show()
+
+    running = True
+
+    def handle_close(event):
+        global running
+        running = False
+
+    fig.canvas.mpl_connect("close_event", handle_close)
+
+    try:
+        while running:
+            frame = cam()
+            img.set_data(frame)
+            img.set_clim(vmin=frame.min(), vmax=frame.max()) # autoscale
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+    finally:
+        del cam
+        print("Camera safely closed.")
