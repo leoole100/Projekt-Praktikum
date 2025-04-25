51d2180f44c42f1294ba85f83e91a844daffdf0f
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2025-04-25 12:51:56 +0200

    designed scanning experiment

--- enable_autoreload.py
+++ enable_autoreload.py
@@ -4,4 +4,6 @@ For developing modules in a IPython enviroment, caching can be disabled with:"
 # %%
 %load_ext autoreload
 %autoreload 2
+
+%matplotlib ipympl
 #%%
---
+++ experiments/scan.py
@@ -0,0 +1,15 @@
+from spectrometer import Spectrometer
+import numpy as np
+import matplotlib as mpl
+import time
+
+s = Spectrometer()
+
+
+wl = np.arange(300, 1200, 100)
+
+for w in wl:
+	s.monochromator.wavelength = w
+	time.sleep(1)
+	s()
+	s.save(f"../measurement/2025-04-25/005 {w}.yaml")
