3d993d22478cac69e85cda2928b690cb2aaf61a5
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2025-05-02 13:06:42 +0200

    first black body curve

---
+++ analysis/spectrum.py
@@ -0,0 +1,29 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("../style.mplstyle")
+import xarray as xr
+from glob import glob
+
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+# %%
+p = sorted(glob("../measurement/2025-05-02/*.asc"))
+
+d = np.loadtxt(p[1])
+bkg = np.loadtxt(p[2])
+
+wl = d[:,0]
+d = d[:,1] - bkg[:,1]
+d /= 2
+
+plt.plot(wl, d)
+plt.xlabel("counts / s")
+plt.ylabel("wavelength (nm)")
+plt.savefig("figures/spectrum.pdf")
+plt.show()
+# %%
